02b5a147dc523d4d3e595deb4902cb2de873c5e3
Daeyeol Ryu <dy@yoobato.com>
2022-03-12 21:32:50 +0900

    Validate Block

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
+  static calculateBlockHash = (
+    index: number, 
+    previousHash: string, 
+    timestamp: number, 
+    data: string
+  ): string => 
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (aBlock: Block): boolean => 
+    typeof aBlock.index === "number" && 
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
   public index: number;
   public hash: string;
   public previousHash: string;
@@ -20,14 +35,6 @@ class Block {
     this.data = data;
     this.timestamp = timestamp;
   }
-  
-  static calculateBlockHash = (
-    index: number, 
-    previousHash: string, 
-    timestamp: number, 
-    data: string
-  ): string => 
-    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 }
 
 const genesisBlock: Block = new Block(0, "1234", "", "Hello", 123456);
@@ -60,6 +67,32 @@ const createNewBlock = (data: string): Block => {
   return newBlock;
 };
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+const getHashForBlock = (aBlock: Block): string => 
+  Block.calculateBlockHash(
+    aBlock.index, 
+    aBlock.previousHash, 
+    aBlock.timestamp, 
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
 
 export {};
