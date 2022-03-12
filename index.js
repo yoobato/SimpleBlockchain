4efe4abd3dcc18b47759062c707ccf039df155f6
Daeyeol Ryu <dy@yoobato.com>
2022-03-12 20:53:11 +0900

    Basic typescript

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi("Yoobato", 31, "male"));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("hello");
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi("Yoobato", 31, "male"));
+
+// 아래처럼 해주지 않으면 이 파일을 모듈로 인식하지 않는다. Typescript 버그인듯.
+export {};
