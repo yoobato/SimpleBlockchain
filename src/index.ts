40bce09857da58e799f9d2f603ad3b397f7cd2b2
Daeyeol Ryu <dy@yoobato.com>
2022-03-12 21:01:57 +0900

    Typescript Interface

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+  name: string,
+  age: number,
+  gender: string,
+}
+
+const person = {
+  name: "Yoobato",
+  age: 31,
+  gender: "male",
 };
 
-console.log(sayHi("Yoobato", 31, "male"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 // 아래처럼 해주지 않으면 이 파일을 모듈로 인식하지 않는다. Typescript 버그인듯.
 export {};
